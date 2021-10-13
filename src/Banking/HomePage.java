ca314ec2aab8297a46202bf2e944fd5d7ccaa6bb
Christian <cmoticska@gmail.com>
2021-10-12 22:22:30 -0400

    Refixing my buggy IDE again

---
+++ src/Banking/HomePage.java
@@ -0,0 +1,17 @@
+package Banking;
+import javafx.fxml.FXML;
+import javafx.scene.input.MouseEvent;
+
+public class HomePage {
+
+    @FXML
+    void goToLoginPage(MouseEvent event) {
+
+    }
+
+    @FXML
+    void goToRequestPage(MouseEvent event) {
+
+    }
+
+}

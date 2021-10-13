7cf58363a7152543a7d0a42308eb53e63661e68d
Christian <cmoticska@gmail.com>
2021-10-13 19:10:24 -0400

    Made RequestAccountPage, added redirection to button

---
+++ src/RequestAccountPage.java
@@ -0,0 +1,28 @@
+import java.io.IOException;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+
+public class RequestAccountPage {
+
+    @FXML
+    private TextField fName;
+    @FXML
+    private TextField lName;
+    @FXML
+    private PasswordField pass;
+    @FXML
+    private Button reqActBtn;
+    @FXML
+    private TextField uName;
+
+    @FXML
+    void sendAndReturn(ActionEvent event) throws IOException {
+        App m = new App();
+        m.changeScene("HomePage.fxml");
+    }
+
+}

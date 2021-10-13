37c9bae80505ab034ba349240524d490d13b5108
Christian <cmoticska@gmail.com>
2021-10-13 19:10:49 -0400

    Added changeScene and fxml functionality

--- src/App.java
+++ src/App.java
@@ -1,5 +1,7 @@
 
 
+import java.io.IOException;
+
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
@@ -8,12 +10,20 @@ import javafx.stage.Stage;
 
 public class App extends Application{
 
+    private static Stage stg;
     @Override
     public void start(Stage primaryStage) throws Exception {
+        stg = primaryStage;
+        primaryStage.setResizable(false);
         Parent root = FXMLLoader.load(getClass().getResource("HomePage.fxml"));
         primaryStage.setTitle("Main Page");
         primaryStage.setScene(new Scene(root));
         primaryStage.show();
+    }
+
+    public void changeScene(String fxml) throws IOException {
+        Parent pane = FXMLLoader.load(getClass().getResource(fxml));
+        stg.getScene().setRoot(pane);
     }
 
     public static void main(String[] args) throws Exception {

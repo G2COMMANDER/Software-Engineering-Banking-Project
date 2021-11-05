011d987eb0adfc6293caf61d7de673b941c441d4
Christian <cmoticska@gmail.com>
2021-11-05 17:31:21 -0400

    Added UserAccountPage and fixed syntax

--- src/HomePage.java
+++ src/HomePage.java
@@ -1,4 +1,5 @@
 import java.io.IOException;
+
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
@@ -7,18 +8,20 @@ public class HomePage {
 
     @FXML
     private Button loginBtn;
+
     @FXML
     private Button reqActBtn;
-
-    @FXML
-    void goToLoginPage(ActionEvent event) {
-
-    }
 
     @FXML
     void goToRequestPage(ActionEvent event) throws IOException {
         App m = new App();
         m.changeScene("RequestAccountPage.fxml");
+    }
+
+    @FXML
+    void goToUserAccountPage(ActionEvent event) throws IOException {
+        App m = new App();
+        m.changeScene("UserAccountPage.fxml");
     }
 
 }
---
+++ src/UserAccountPage.java
@@ -0,0 +1,42 @@
+import java.io.IOException;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+
+public class UserAccountPage {
+
+    @FXML
+    private Button depositMoneyBtn;
+
+    @FXML
+    private Button logOutBtn;
+
+    @FXML
+    private Button transferMoneyBtn;
+
+    @FXML
+    private Button withdrawMoneyBtn;
+
+    @FXML
+    void goToDepositPage(ActionEvent event) {
+
+    }
+
+    @FXML
+    void goToTransferPage(ActionEvent event) {
+
+    }
+
+    @FXML
+    void goToWithdrawPage(ActionEvent event) {
+
+    }
+
+    @FXML
+    void logOut(ActionEvent event) throws IOException {
+        App m = new App();
+        m.changeScene("HomePage.fxml");
+    }
+
+}

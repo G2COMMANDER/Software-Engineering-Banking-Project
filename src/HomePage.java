1cbdb8f8827538f6cc7475594f5482c218f14fa2
Christian <cmoticska@gmail.com>
2021-11-05 17:56:01 -0400

    More button id removing

--- src/HomePage.java
+++ src/HomePage.java
@@ -2,14 +2,8 @@ import java.io.IOException;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.scene.control.Button;
 
 public class HomePage {
-
-    @FXML
-    private Button loginBtn;
-    @FXML
-    private Button reqActBtn;
 
     App m = new App();
 

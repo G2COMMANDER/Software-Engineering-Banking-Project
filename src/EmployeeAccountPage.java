e6f32598874c48c98ef2b75193f27bebde6648dc
Christian <cmoticska@gmail.com>
2021-11-05 20:40:01 -0400

    Created employee account page

---
+++ src/EmployeeAccountPage.java
@@ -0,0 +1,30 @@
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+
+public class EmployeeAccountPage {
+
+    @FXML
+    private Label name;
+
+    @FXML
+    void goToAcceptDenyPage(ActionEvent event) {
+
+    }
+
+    @FXML
+    void goToDeleteAccountsPage(ActionEvent event) {
+
+    }
+
+    @FXML
+    void goToTransactionHistoryPage(ActionEvent event) {
+
+    }
+
+    @FXML
+    void logOut(ActionEvent event) {
+
+    }
+
+}

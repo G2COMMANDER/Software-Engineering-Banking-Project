de4aedaa8bdcecf4589786ebd11852583f4ea193
Christian <cmoticska@gmail.com>
2021-11-05 20:38:45 -0400

    Added id's to name and balance label

--- src/UserAccountPage.java
+++ src/UserAccountPage.java
@@ -2,10 +2,16 @@ import java.io.IOException;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.Label;
 
 public class UserAccountPage {
 
     App m = new App();
+
+    @FXML
+    private Label balanceAmount;
+    @FXML
+    private Label name;
 
     @FXML
     void goToDepositPage(ActionEvent event) {

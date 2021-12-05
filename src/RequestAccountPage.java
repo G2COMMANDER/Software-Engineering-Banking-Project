731cbe854f343bdf527da03ec2f772117bd0713b
Christian <cmoticska@gmail.com>
2021-12-05 01:27:24 -0500

    Added entire system to check fields

--- src/RequestAccountPage.java
+++ src/RequestAccountPage.java
@@ -1,4 +1,11 @@
+import java.io.File;
+import java.io.FileInputStream;
 import java.io.IOException;
+
+import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.xssf.usermodel.XSSFSheet;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
@@ -8,18 +15,92 @@ import javafx.scene.control.TextField;
 public class RequestAccountPage {
 
     @FXML
-    private TextField fName;
+    private TextField userName;
     @FXML
-    private TextField lName;
+    private TextField firstName;
     @FXML
-    private PasswordField pass;
+    private TextField lastName;
     @FXML
-    private TextField uName;
+    private PasswordField password;
+    @FXML
+    private PasswordField checkPassword;
+
 
     App m = new App();
+
+    private boolean checkRequestedAccount(String uNameC, String fNameC, String lNameC, String passC, String checkPassC) {
+        
+        boolean checkReqAccCreds = true;
+        String tmpUName;
+        String tmpFName;
+        String tmpLName;
+
+        try {
+            FileInputStream file = new FileInputStream(new File("src/Database/Database.xlsx"));
+
+            // Create Workbook instance holding reference to Database.xlsx file
+            XSSFWorkbook workbook = new XSSFWorkbook(file);
+
+            // Get UserList sheet from Database.xlsx
+            for (int i = 0; i < 1; i++) {
+                XSSFSheet sheet = workbook.getSheetAt(i);
+                System.out.println(sheet.getSheetName());
+
+                // this checks the username, firstname, and lastname
+                for (Row row : sheet) { // For each Row.
+                    
+                    Cell uNameCell = row.getCell(0); // this is the username cell
+                    Cell fNameCell = row.getCell(1); // this is the first name cell
+                    Cell lNameCell = row.getCell(2); // this is the last name cell
+
+                    tmpUName = uNameCell.getStringCellValue();
+                    tmpFName = fNameCell.getStringCellValue();
+                    tmpLName = lNameCell.getStringCellValue();
+
+                    if (fNameC.equals(tmpFName) && lNameC.equals(tmpLName)) { // checks is first and last name exist
+                        checkReqAccCreds = false;
+                        System.out.println(fNameCell.getColumnIndex() + " This user already exists: " + fNameCell.getStringCellValue() + " " + lNameCell.getStringCellValue());
+                    } else if (uNameC.equals(tmpUName)) { // checks if the username already exists
+                        checkReqAccCreds = false;
+                        System.out.println(uNameCell.getColumnIndex() + " Username already exists: " + uNameCell.getStringCellValue());
+                    } else if (!passC.equals(checkPassC)) { // checks if the passwords match
+                        checkReqAccCreds = false;
+                        System.out.println("Passwords do not match, please try again.");
+                    } else {
+                        continue;
+                    }
+                }
+            }
+
+            file.close();
+            workbook.close();
+
+        } catch (Exception e) {
+            checkReqAccCreds = false;
+            System.out.println("Something broke?");
+        }
+        
+        return checkReqAccCreds;
+    }
     
     @FXML
-    void sendAndReturn(ActionEvent event) throws IOException {
+    void requestAccount(ActionEvent event) throws IOException {
+
+        String uName = userName.getText().toString();
+        String fName = firstName.getText().toString();
+        String lName = lastName.getText().toString();
+        String pass = password.getText().toString();
+        String checkPass = checkPassword.getText().toString();
+
+        if (checkRequestedAccount(uName, fName, lName, pass, checkPass) == true) {
+            System.out.println("Account created successfully");
+        } else {
+            System.out.println("Please try again");
+        }
+    }
+
+    @FXML
+    void returnHome(ActionEvent event) throws IOException {
         m.changeScene("fxml_pages/HomePage.fxml");
     }
 

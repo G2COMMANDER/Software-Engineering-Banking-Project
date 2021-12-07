d4325e200198aca79de13976deb5e62d06a266fc
Christian <cmoticska@gmail.com>
2021-12-06 21:30:27 -0500

    Added all the code to view transaction history

--- src/TransactionHistoryPage.java
+++ src/TransactionHistoryPage.java
@@ -1,20 +1,63 @@
-import java.io.IOException;
+import com.gembox.spreadsheet.*;
+import javafx.beans.property.ReadOnlyObjectWrapper;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.scene.control.TableView;
+import javafx.scene.control.*;
+import javafx.scene.control.cell.TextFieldTableCell;
+import javafx.stage.FileChooser;
+import java.io.File;
+import java.io.IOException;
 
 public class TransactionHistoryPage {
 
     App m = new App();
 
     @FXML
-    private TableView<?> table;
+    private TableView table;
 
     @FXML
-    void displayDeposits(ActionEvent event) {
+    void displayDeposits(ActionEvent event) throws IOException {
+        FileChooser fileChooser = new FileChooser();
+        fileChooser.setTitle("Open file");
+        File file = fileChooser.showOpenDialog(table.getScene().getWindow());
 
+        ExcelFile workbook = ExcelFile.load(file.getAbsolutePath());
+        ExcelWorksheet worksheet = workbook.getWorksheet(0);
+        String[][] sourceData = new String[100][26];
+        for (int row = 0; row < sourceData.length; row++) {
+            for (int column = 0; column < sourceData[row].length; column++) {
+                ExcelCell cell = worksheet.getCell(row, column);
+                if (cell.getValueType() != CellValueType.NULL)
+                    sourceData[row][column] = cell.getValue().toString();
+            }
+        }
+        fillTable(sourceData);
     }
 
+    private void fillTable(String[][] dataSource) {
+        table.getColumns().clear();
+
+        ObservableList<ObservableList<String>> data = FXCollections.observableArrayList();
+        for (String[] row : dataSource)
+            data.add(FXCollections.observableArrayList(row));
+        table.setItems(data);
+
+        for (int i = 0; i < dataSource[0].length; i++) {
+            final int currentColumn = i;
+            TableColumn<ObservableList<String>, String> column = new TableColumn<>(ExcelColumnCollection.columnIndexToName(currentColumn));
+            column.setCellValueFactory(param -> new ReadOnlyObjectWrapper<>(param.getValue().get(currentColumn)));
+            column.setEditable(true);
+            column.setCellFactory(TextFieldTableCell.forTableColumn());
+            column.setOnEditCommit(
+                    (TableColumn.CellEditEvent<ObservableList<String>, String> t) -> {
+                        t.getTableView().getItems().get(t.getTablePosition().getRow()).set(t.getTablePosition().getColumn(), t.getNewValue());
+                    });
+            table.getColumns().add(column);
+        }
+    }
+    
     @FXML
     void displayWithdrawls(ActionEvent event) {
 

e5a8bbec590585840301f9f2c697e49db4709d37
Christian <cmoticska@gmail.com>
2021-12-07 00:31:43 -0500

    Added method to "go back"

--- src/UserListPage.java
+++ src/UserListPage.java
@@ -1,8 +1,12 @@
+import java.io.IOException;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 
 public class UserListPage {
+
+    App m = new App();
 
     @FXML
     private TableColumn<?, ?> cardInfoCol;
@@ -20,5 +24,10 @@ public class UserListPage {
     private TableView<?> userList;
     @FXML
     private TableColumn<?, ?> userNameCol;
+
+    @FXML
+    void returnHome(ActionEvent event) throws IOException {
+        m.changeScene("fxml_pages/employee_pages/EmployeeAccountPage.fxml");
+    }
 
 }
